38aceba3274b6ecd27866712a224f72d83ede945
MadhuSudhan <smr662006@gmail.com>
2026-03-30 22:34:05 +0530

    update

--- patterns/Triangle.java
+++ patterns/Triangle.java
@@ -4,7 +4,6 @@ public class Triangle {
       for(int i=1;i<=n;i++){
         for(int j=1;j<=i;j++){
           System.out.print("* ");
-          
         }
         System.out.println("");
       }
